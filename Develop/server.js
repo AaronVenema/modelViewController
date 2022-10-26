091dbb47d079629483bcff8cceac9cf52265d840
AaronVenema <34764586+AaronVenema@users.noreply.github.com>
2022-10-26 18:01:52 -0500

    index route fixed, handlebars and sessions commented out for testing

--- Develop/server.js
+++ Develop/server.js
@@ -1,30 +1,30 @@
 const express = require('express');
-const session = require('express-session');
+// const session = require('express-session');
 const routes = require('./controllers');
-const path =require('path');
-const expressHandlebars = ('express-handlebars');
-const helpers = require('./utils/helpers')
+// const path =require('path');
+// const expressHandlebars = ('express-handlebars');
+// const helpers = require('./utils/helpers')
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
-const handlebars = expressHandlebars.create({ helpers}) 
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
+// const handlebars = expressHandlebars.create({ helpers}) 
+// const sess = {
+//   secret: 'Super secret secret',
+//   cookie: {},
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize
+//   })
+// };
 
-app.use(session(sess));
-app.engine('handlerbars', handlebars.engine)
-app.set('view engine', 'handlebars')
+// app.use(session(sess));
+// app.engine('handlerbars', handlebars.engine)
+// app.set('view engine', 'handlebars')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
