f21e1bcfe82f407be6e53f24e2669f5d7b289ebc
AaronVenema <34764586+AaronVenema@users.noreply.github.com>
2022-10-26 16:49:48 -0500

    more boilers added

--- Develop/controllers/api/commentRoutes.js
+++ Develop/controllers/api/commentRoutes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Comment } = require('../../models');
 
 router.get('/', async (req, res) => {
   try {
-    const ProjectData = await Project.findAll({
-      include: [ {model: Project} ],
+    const CommentData = await Comment.findAll({
+      include: [ {model: Comment} ],
     });
 
-    res.status(200).json(ProjectData);
+    res.status(200).json(CommentData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -17,12 +17,12 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
-    const newProject = await Project.create({
+    const newComment = await Comment.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newComment);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -30,19 +30,19 @@ router.post('/', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    const CommentData = await Comment.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+    if (!CommentData) {
+      res.status(404).json({ message: 'No Comment found with this id!' });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(CommentData);
   } catch (err) {
     res.status(500).json(err);
   }
