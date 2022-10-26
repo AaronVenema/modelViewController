a55cfd790dc1b99b5781db83654fb6c88955d12f
AaronVenema <34764586+AaronVenema@users.noreply.github.com>
2022-10-26 17:57:31 -0500

    i think server is correct

--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
-
+const path =require('path');
+const expressHandlebars = ('express-handlebars');
+const helpers = require('./utils/helpers')
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+const handlebars = expressHandlebars.create({ helpers}) 
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
@@ -19,10 +20,11 @@ const sess = {
 };
 
 app.use(session(sess));
-
+app.engine('handlerbars', handlebars.engine)
+app.set('view engine', 'handlebars')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
